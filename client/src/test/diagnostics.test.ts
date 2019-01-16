15bee613bc03cc06e63f5646a58903f0f8ffdb87
Shintaro Kojima <goodies@codeout.net>
2019-01-17 02:37:09 +0900

    Add tests for validation

--- client/src/test/diagnostics.test.ts
+++ client/src/test/diagnostics.test.ts
@@ -1,41 +1,93 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
 import * as vscode from 'vscode'
 import * as assert from 'assert'
-import { getDocUri, activate } from './helper'
+import {getDocUri, activate} from './helper'
 
 describe('Should get diagnostics', () => {
-  const docUri = getDocUri('diagnostics.txt')
+    const docUri = getDocUri('junos.conf');
 
-  it('Diagnoses uppercase texts', async () => {
-    await testDiagnostics(docUri, [
-      { message: 'ANY is all uppercase.', range: toRange(0, 0, 0, 3), severity: vscode.DiagnosticSeverity.Warning, source: 'ex' },
-      { message: 'ANY is all uppercase.', range: toRange(0, 14, 0, 17), severity: vscode.DiagnosticSeverity.Warning, source: 'ex' },
-      { message: 'OS is all uppercase.', range: toRange(0, 18, 0, 20), severity: vscode.DiagnosticSeverity.Warning, source: 'ex' }
-    ])
-  })
-})
+    it('Diagnoses syntax', async () => {
+        await testDiagnostics(docUri, [
+            {
+                message: '"set interfaces xe-0/0/0 " is invalid',
+                range: toRange(2, 0, 2, 24),
+                severity: vscode.DiagnosticSeverity.Error,
+                source: 'ex'
+            },
+            {
+                message: '"foo-filter_" is not defined',
+                range: toRange(14, 56, 14, 67),
+                severity: vscode.DiagnosticSeverity.Error,
+                source: 'ex'
+            },
+            {
+                message: '"inet_" is invalid',
+                range: toRange(15, 38, 15, 43),
+                severity: vscode.DiagnosticSeverity.Error,
+                source: 'ex'
+            },
+            {
+                message: '"foo-statement_" is not defined',
+                range: toRange(18, 41, 18, 55),
+                severity: vscode.DiagnosticSeverity.Error,
+                source: 'ex'
+            },
+            {
+                message: '"xe-0/0/0.1" is not defined',
+                range: toRange(20, 29, 20, 39),
+                severity: vscode.DiagnosticSeverity.Error,
+                source: 'ex'
+            },
+            {
+                message: '"protocols" is invalid',
+                range: toRange(22, 4, 22, 13),
+                severity: vscode.DiagnosticSeverity.Error,
+                source: 'ex'
+            },
+            {
+                message: '"foo-prefix_" is not defined',
+                range: toRange(29, 67, 29, 78),
+                severity: vscode.DiagnosticSeverity.Error,
+                source: 'ex'
+            },
+            {
+                message: '"foo-community_" is not defined',
+                range: toRange(31, 65, 31, 79),
+                severity: vscode.DiagnosticSeverity.Error,
+                source: 'ex'
+            },
+            {
+                message: '"foo-as-path_" is not defined',
+                range: toRange(33, 63, 33, 75),
+                severity: vscode.DiagnosticSeverity.Error,
+                source: 'ex'
+            },
+            {
+                message: '"foo-as-path-group_" is not defined',
+                range: toRange(35, 69, 35, 87),
+                severity: vscode.DiagnosticSeverity.Error,
+                source: 'ex'
+            },
+        ])
+    })
+});
 
 function toRange(sLine: number, sChar: number, eLine: number, eChar: number) {
-  const start = new vscode.Position(sLine, sChar)
-  const end = new vscode.Position(eLine, eChar)
-  return new vscode.Range(start, end)
+    const start = new vscode.Position(sLine, sChar);
+    const end = new vscode.Position(eLine, eChar);
+    return new vscode.Range(start, end)
 }
 
 async function testDiagnostics(docUri: vscode.Uri, expectedDiagnostics: vscode.Diagnostic[]) {
-  await activate(docUri)
+    await activate(docUri);
 
-  const actualDiagnostics = vscode.languages.getDiagnostics(docUri);
+    const actualDiagnostics = vscode.languages.getDiagnostics(docUri);
 
-  assert.equal(actualDiagnostics.length, expectedDiagnostics.length);
+    assert.equal(actualDiagnostics.length, expectedDiagnostics.length);
 
-  expectedDiagnostics.forEach((expectedDiagnostic, i) => {
-    const actualDiagnostic = actualDiagnostics[i]
-    assert.equal(actualDiagnostic.message, expectedDiagnostic.message)
-    assert.deepEqual(actualDiagnostic.range, expectedDiagnostic.range)
-    assert.equal(actualDiagnostic.severity, expectedDiagnostic.severity)
-  })
+    expectedDiagnostics.forEach((expectedDiagnostic, i) => {
+        const actualDiagnostic = actualDiagnostics[i];
+        assert.equal(actualDiagnostic.message, expectedDiagnostic.message);
+        assert.deepEqual(actualDiagnostic.range, expectedDiagnostic.range);
+        assert.equal(actualDiagnostic.severity, expectedDiagnostic.severity);
+    })
 }
