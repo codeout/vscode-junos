1987455f4912dda081e705b5ca716fe97e9fb4b1
Shintaro Kojima <goodies@codeout.net>
2019-01-18 05:15:33 +0900

    Apply new definitions data structure to defined symbol based completion

--- server/src/completion.ts
+++ server/src/completion.ts
@@ -13,10 +13,8 @@ import {Session} from './session';
 
 export function completion(session: Session): RequestHandler<TextDocumentPositionParams, CompletionItem[], void> {
     return (textDocumentPosition: TextDocumentPositionParams): CompletionItem[] => {
-        // The pass parameter contains the position of the text document in
-        // which code complete got requested. For the example we ignore this
-        // info and always provide the same completion items.
-        const doc = session.documents.get(textDocumentPosition.textDocument.uri);
+        const uri = textDocumentPosition.textDocument.uri;
+        const doc = session.documents.get(uri);
         let line = doc.getText().split("\n")[textDocumentPosition.position.line];
 
         if (!line.match(prefixPattern)) {
@@ -26,21 +24,21 @@ export function completion(session: Session): RequestHandler<TextDocumentPositio
         line = line.replace(prefixPattern, '');
         const keywords = session.parser.keywords(line);
 
-        // List defined symbold
+        // List defined symbols
         if (line.match(/\s+interface\s+$/)) {
-            addReferences(session, session.definitions['interface'], keywords);
+            addReferences(session, session.definitions.getDefinitions(uri, 'interface'), keywords);
         } else if (line.match(/\s+from\s+(?:source-|destination-)?prefix-list\s+$/)) {
-            addReferences(session, session.definitions['prefix-list'], keywords);
+            addReferences(session, session.definitions.getDefinitions(uri, 'prefix-list'), keywords);
         } else if (line.match(/\s+(?:import|export)\s+$/)) {
-            addReferences(session, session.definitions['policy-statement'], keywords);
+            addReferences(session, session.definitions.getDefinitions(uri, 'policy-statement'), keywords);
         } else if (line.match(/\s+(?:from\s+community|then\s+community\s+(?:add|delete|set))\s+$/)) {
-            addReferences(session, session.definitions['community'], keywords);
+            addReferences(session, session.definitions.getDefinitions(uri, 'community'), keywords);
         } else if (line.match(/\s+from\s+as-path\s+$/)) {
-            addReferences(session, session.definitions['as-path'], keywords);
+            addReferences(session, session.definitions.getDefinitions(uri, 'as-path'), keywords);
         } else if (line.match(/\s+from\s+as-path-group\s+$/)) {
-            addReferences(session, session.definitions['as-path-group'], keywords);
+            addReferences(session, session.definitions.getDefinitions(uri, 'as-path-group'), keywords);
         } else if (line.match(/\s+filter\s+(?:input|output|input-list|output-list)\s+$/)) {
-            addReferences(session, session.definitions['firewall-filter'], keywords);
+            addReferences(session, session.definitions.getDefinitions(uri, 'firewall-filter'), keywords);
         }
 
         return keywords.map(keyword => ({
