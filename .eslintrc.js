a3eefa67308d4f63483ad542054ca0b77994533a
Shintaro Kojima <goodies@codeout.net>
2021-05-05 13:54:03 +0900

    Configure eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,6 @@
 /**@type {import('eslint').Linter.Config} */
+/* eslint indent: ["error", "tab"] */
+
 // eslint-disable-next-line no-undef
 module.exports = {
 	root: true,
@@ -11,10 +13,31 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended',
 	],
 	rules: {
-		'semi': [2, "always"],
+		'semi': [2, 'always'],
 		'@typescript-eslint/no-unused-vars': 0,
 		'@typescript-eslint/no-explicit-any': 0,
 		'@typescript-eslint/explicit-module-boundary-types': 0,
 		'@typescript-eslint/no-non-null-assertion': 0,
-	}
+	},
+
+	'overrides': [
+		{
+			files: [
+				'client/src/test/*.test.ts',
+				'server/src/*.ts',
+			],
+			extends: [
+				'plugin:@typescript-eslint/eslint-recommended',
+				'prettier',
+				'plugin:prettier/recommended',
+			],
+			rules: {
+				'camelcase': [2, {'properties': 'always'}],
+				'sort-imports': [2, {
+					'ignoreCase': false,
+					'allowSeparatedGroups': true,
+				}],
+			}
+		}
+	]
 };
